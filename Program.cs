83041f92f0a80d94accc6ffb74df185124736ca9
stas9025 <berezovskiy1991@yandex.ru>
2022-09-06 22:22:42 +0300

    first commit_metod_array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] arr1 = new string[7] { "Russia", "1234", "hello", "-2", ":)", "5", "world" };
+string[] arr2 = new string[arr1.Length];
+
+void Array(string[] arr1, string[] arr2)
+{
+  int count = 0;
+  for (int i = 0; i < arr1.Length; i++)
+  {
+    if (arr1[i].Length <= 3)
+    {
+      arr2[count] = arr1[i];
+      count++;
+    }
+  }
+  Console.WriteLine("[{0}]", string.Join(", ", arr1));
+}
+
+
